1830f9a231bd0055f85e41436d9a26b9ad2f1666
Andreas Blombäck <andreas@blomback.se>
2015-05-13 12:24:03 +0200

    Initial commit

---
+++ gulpfile.js
@@ -0,0 +1,70 @@
+var gulp         = require('gulp'),
+	sass         = require('gulp-sass'),
+	autoprefixer = require('gulp-autoprefixer'),
+	uglify       = require('gulp-uglify'),
+	jshint       = require('gulp-jshint'),
+	header       = require('gulp-header'),
+	rename       = require('gulp-rename'),
+	minifyCSS    = require('gulp-minify-css'),
+	gutil        = require('gulp-util'),
+	sourcemaps   = require('gulp-sourcemaps'),
+	package      = require('./package.json'),
+	paths        = require('./paths.json'),
+	concat       = require('gulp-concat');
+
+
+var banner = [
+	'/*!\n' +
+	' * <%= package.title %>\n' +
+	' * <%= package.url %>\n' +
+	' * @author <%= package.author %>\n' +
+	' * @version <%= package.version %>\n' +
+	' * Copyright ' + new Date().getFullYear() + '. <%= package.license %> licensed.\n' +
+	' */',
+	'\n'
+].join('');
+
+gulp.task('css', function()
+{
+	gulp.src(paths.css.src)
+		.pipe( sass() )
+		.on('error', function(err)
+		{
+			gutil.log("Error: ", err.message);
+			gutil.log("File: ", err.fileName);
+			gutil.log("Line: ", err.lineNumber);
+			gutil.beep();
+		})
+		.pipe(autoprefixer('last 4 version'))
+		.pipe(gulp.dest(paths.css.dest))
+		.pipe(minifyCSS())
+		.pipe(rename({suffix: '.min'}))
+		.pipe(header(banner, {package: package}))
+		.pipe(gulp.dest(paths.css.dest))
+	;
+});
+
+gulp.task('js', function()
+{
+	gulp.src(paths.js.src)
+		.pipe(concat('all.js'))
+		.pipe(gulp.dest(paths.js.dest))
+		.pipe(uglify())
+		.on('error', function(err)
+		{
+			gutil.log("Error: ", err.message);
+			gutil.log("File: ", err.fileName);
+			gutil.log("Line: ", err.lineNumber);
+			gutil.beep();
+		})
+		.pipe(header(banner, {package: package}))
+		.pipe(rename({suffix: '.min'}))
+		.pipe(gulp.dest(paths.js.dest))
+	;
+});
+
+gulp.task('default', ['css', 'js'], function()
+{
+	gulp.watch(paths.css.watch, ['css']);
+	gulp.watch(paths.js.watch, ['js']);
+});
---
+++ paths.js
@@ -0,0 +1,26 @@
+{
+	"js": {
+		"watch": [
+			"resources/assets/js/vendor/**/*.js",
+			"resources/assets/js/*.js"
+		],
+		"src": [
+			"resources/assets/js/vendor/jquery.min.js",
+			"resources/assets/js/vendor/jquery.*.js",
+			"resources/assets/js/vendor/*.js",
+			"resources/assets/js/*.js"
+		],
+		"dest": "public/js/"
+	},
+
+	"css": {
+		"watch": [
+			"resources/assets/sass/*.scss",
+			"resources/assets/sass/*.sass"
+		],
+		"src": [
+			"resources/assets/sass/style.scss"
+		],
+		"dest": "public/css/"
+	}
+}
